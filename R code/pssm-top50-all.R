3231a3c9ee8890106934f3c0a240fc7ca95c1c7c
Eclipse-LiZhao <lizhao.working@hotmail.com>
2015-07-07 15:25:21 +0800

    add top-50-all pssm

--- R code/pssm-top50-all.R
+++ R code/pssm-top50-all.R
@@ -10,8 +10,14 @@ for(i in 1:7)
   sigmar<-tabler[c(1,i+1)]
   newsigmal<-sigmal[order(sigmal[,2],decreasing=TRUE),]
   newsigmar<-sigmar[order(sigmar[,2],decreasing=TRUE),]
+  seqtop500<-"XXX"
+  seqtop500[500]<-"XXX"
+  for(j in 1:500)
   {
+    seqtop500[j]=paste(as.character(newsigmal[j,1]),as.character(newsigmar[j,1]),sep="")
   }
+  seqtop500=as.data.frame(seqtop500)
+  outname<-paste("d:/top50/",type,"-all.txt",sep="")
+  write.table(seqtop500, file = outname, row.names = F, quote = F)
 }
-
 
